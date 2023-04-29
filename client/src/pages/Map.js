94279342b532985cdacd4bf2109f3ff7d1dd4cad
Christopher Mena <mena.christopher.student@gmail.com>
2023-04-29 17:43:56 -0400

    working map with custom styling

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,30 +1,35 @@
-import React from 'react'
+import React, { useMemo, useRef } from 'react'
 import { useState, useCallback, memo } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '400px',
   height: '400px'
 };
 
-const center = {
-    lat: 40.7678,
-    lng: -73.9645
-};
 
 const Map = () => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY
   })
-
+  const center = useMemo(() =>({lat: 40.7678,lng: -73.9645}), []);
+  const options = useMemo(() => ({
+    disableDefaultUI: true,
+    clickableIcons: false,
+    mapId: "a15b2059c20005a5",
+  }), 
+  []);
+  const [office, setOffice] = useState({});
+  const mapRef = useRef();
   const [map, setMap] = useState(null)
 
   const onLoad = useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-
+    mapRef.current = map;
+    getCurrentLocation();
     setMap(map)
   }, [])
 
@@ -32,19 +37,32 @@ const Map = () => {
     setMap(null)
   }, [])
 
-  console.log(process.env);
+  const getCurrentLocation = () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+        setOffice(pos);
+      })
+    }
+  }
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={15}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      defaultCenter={center}
+      zoom={15}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      options={options}
+    >
+      {office &&
+      <Marker position={office} icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" />}
+      <></>
+    </GoogleMap>
+  ) : <div>Loading...</div>;
+  
 }
 
 export default memo(Map)
